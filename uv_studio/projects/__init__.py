afdc65437ea66ee4e7570c91fea4c3c1d4c1346d
Богдан Гайдук <FedoTot1327@yandex.ru>
2026-08-11 13:13:36 +0300

    feat: export project media range contracts

--- uv_studio/projects/__init__.py
+++ uv_studio/projects/__init__.py
@@ -9,17 +9,27 @@ from .archive import (
     export_project,
     import_project,
 )
+from .media_ranges import (
+    MEDIA_RANGE_SCHEMA_VERSION,
+    MICROSECONDS_PER_SECOND,
+    ProjectMediaRange,
+    ResolvedProjectMediaRange,
+)
 from .models import PROJECT_SCHEMA_VERSION, ProjectDocument, ProjectReference
 from .store import ProjectStore
 
 __all__ = [
     "ARCHIVE_SCHEMA_VERSION",
     "ArchiveLimits",
+    "MEDIA_RANGE_SCHEMA_VERSION",
+    "MICROSECONDS_PER_SECOND",
     "PROJECT_SCHEMA_VERSION",
     "ProjectArchiveError",
     "ProjectDocument",
+    "ProjectMediaRange",
     "ProjectReference",
     "ProjectStore",
+    "ResolvedProjectMediaRange",
     "UnsupportedArchiveSchema",
     "create_backup",
     "export_project",
