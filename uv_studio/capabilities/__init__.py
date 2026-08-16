abaacdc86c52eee6db93ee677a6c6fff48d0e1d4
Богдан Гайдук <FedoTot1327@yandex.ru>
2026-08-16 16:19:56 +0300

    Register Music Video render capability

--- uv_studio/capabilities/__init__.py
+++ uv_studio/capabilities/__init__.py
@@ -54,6 +54,7 @@ def build_builtin_capability_registry() -> CapabilityRegistry:
     from .adapters.audio_loudness import register_audio_loudness_capability
     from .adapters.dubbing_render import register_dubbing_render_capability
     from .adapters.edit_render import register_edit_render_capability
+    from .adapters.music_video_render import register_music_video_render_capability
     from .adapters.webvtt_subtitles import register_webvtt_subtitle_adapter
     from .adapters.whisperx_alignment import register_whisperx_alignment_adapter
 
@@ -61,6 +62,7 @@ def build_builtin_capability_registry() -> CapabilityRegistry:
     register_artifact_preview_capability(registry)
     register_audio_loudness_capability(registry)
     register_dubbing_render_capability(registry)
+    register_music_video_render_capability(registry)
     register_argos_translate_adapter(registry)
     register_whisperx_alignment_adapter(registry)
     register_webvtt_subtitle_adapter(registry)
